4481aa47f3587f5036d047769154f57587872fb2
HossainKhademian <hco@post.com>
2017-12-13 13:16:00 -0800

    bintray release config

--- src/main/kotlin/ir/hossainco/liveevent/LiveEventUtil.kt
+++ src/main/kotlin/ir/hossainco/liveevent/LiveEventUtil.kt
@@ -3,12 +3,12 @@ package ir.hossainco.liveevent
 import android.arch.lifecycle.LifecycleOwner
 import android.arch.lifecycle.Observer
 
-private fun <T> toObserver(observer: (T?) -> Any): Observer<T> =
-	Observer { observer(it) }
+private fun <T> toObserver(observer: (T) -> Any): Observer<T> =
+	Observer { it?.let(observer) }
 
 fun <T> liveEvent(clazz: Class<LiveEvent<T>>? = null) = LiveEventProperty(clazz)
 
-fun <T> liveEvent(owner: LifecycleOwner? = null, clazz: Class<LiveEvent<T>>? = null, observer: Observer<T?>) =
+fun <T> liveEvent(owner: LifecycleOwner? = null, clazz: Class<LiveEvent<T>>? = null, observer: Observer<T>) =
 	liveEvent(clazz).apply {
 		if (owner != null)
 			liveEvent.observe(owner, observer)
@@ -16,5 +16,5 @@ fun <T> liveEvent(owner: LifecycleOwner? = null, clazz: Class<LiveEvent<T>>? = n
 			liveEvent.observeForever(observer)
 	}
 
-fun <T> liveEvent(owner: LifecycleOwner? = null, clazz: Class<LiveEvent<T>>? = null, observer: (T?) -> Any) =
+fun <T> liveEvent(owner: LifecycleOwner? = null, clazz: Class<LiveEvent<T>>? = null, observer: (T) -> Any) =
 	liveEvent(owner, clazz, toObserver(observer))
